b5c6207bbac1e5596cc20f2763b3958004e5fb16
Ege Uzun <38254705+eegeuzunn@users.noreply.github.com>
2025-02-18 23:38:53 +0300

    add api gateway module

---
+++ user-service/src/main/java/com/userservice/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.userservice.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping()
+public class UserController {
+
+    @GetMapping
+    public void getUserById (){
+
+    }
+
+    @PostMapping
+    public void postAUser(){
+
+    }
+
+    @PutMapping
+    public void updateAUser(){
+
+    }
+
+    @DeleteMapping
+    public void deleteAUser(){
+
+    }
+
+
+}
