6935f0dde2b362e6e7388e10fb825b8705c33f4d
Ege Uzun <38254705+eegeuzunn@users.noreply.github.com>
2025-03-03 00:50:33 +0300

    order service init

--- payment-service/src/main/java/com/paymentservice/controller/PaymentController.java
+++ payment-service/src/main/java/com/paymentservice/controller/PaymentController.java
@@ -1,9 +1,10 @@
 package com.paymentservice.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
 
-    @Get
+    @PostMapping("/payment/value/{value}/cartId/{cartId}")
 }
